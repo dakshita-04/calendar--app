e8b9c94278374564acc8648ec6bdbb5b37a3bc49
QITServices <qit.harika@gmail.com>
2023-02-09 19:24:07 +0530

    first commit

--- src/FormMain.js
+++ src/FormMain.js
@@ -31,6 +31,11 @@ import Validator, {
 } from "devextreme-react/validator";
 import { Popup } from "devextreme-react/popup";
 import ValidationGroup from "devextreme-react/validation-group";
+import "react-phone-number-input/style.css";
+// import { useUserAuth } from "./UserAuth";
+import {useUserAuth} from './UserAuth'
+import "react-phone-number-input/style.css";
+import { Link, useNavigate } from "react-router-dom";
 
 const FormMain = () => {
   const [firstName, setFirstName] = useState("");
@@ -41,11 +46,52 @@ const FormMain = () => {
   const [enterOtp, setEnterOtp] = useState("");
   const [companyName, setCompanyName] = useState("");
   const [subject, setSubject] = useState("");
-
+  // for otp verication
+  const [error, setError] = useState("");
+  const [number, setNumber] = useState("");
+  const [flag, setFlag] = useState(false);
+  const [otp, setOtp] = useState("");
+  const [result, setResult] = useState("");
+  const { setUpRecaptha } = useUserAuth();
+  const navigate = useNavigate();
   const namePattern = /^[^0-9]+$/;
   const phonePattern = /^[02-9]\d{9}$/;
   const phoneRules = {
     X: /[02-9]/,
+  };
+  // for otp verification
+  const getOtp = async (e) => {
+    e.preventDefault();
+    console.log(number);
+    setError("");
+    if (number === "" || number === undefined) {
+      return alert("Please enter a valid Phone Number...");
+    }
+    //return setError("Please enter a valid Phone Number...");
+    //return alert("Please enter a valid Phone Number...");
+    try {
+      const response = await setUpRecaptha(number);
+      setResult(response);
+      setFlag(true);
+    } catch (err) {
+      //alert("Too many Phone OTP Requests From Browser Today");
+      //setError(err.message);
+      console.log(err.message);
+    }
+  };
+
+  const verifyOtp = async (e) => {
+    e.preventDefault();
+    setError("");
+    if (otp === "" || otp === null) return;
+    try {
+      await result.confirm(otp);
+      navigate("/dashboard");
+    } catch (err) {
+      alert("Please Enter Valid OTP");
+      // setError(err.message);
+      console.log(err.message);
+    }
   };
 
   const scheduleEvent = async (e) => {
@@ -92,185 +138,217 @@ const FormMain = () => {
       <div className="long-title">
         <h3>Enter the Details</h3>
       </div>
+      <div style={{marginRight:"20px"}}>
       <ValidationGroup>
-        <Form
-          colCount={2}
-          labelMode="floating"
-          labelLocation="left"
-          // onContentReady={validateForm}
-          //label="Select the"
+      <Form
+        colCount={2}
+        labelMode="floating"
+        labelLocation="left"
+        // onContentReady={validateForm}
+        //label="Select the"
+        
+      >
+        <GroupItem
+        //caption="Enter the Following Fields"
+        //style={{ padding: "20px", backgroundColor: "black" }}
         >
-          <GroupItem
-          //caption="Enter the Following Fields"
+          <TextBox
+            label="First Name"
+            labelMode="floating"
+            defaultValue={firstName}
+            onValueChanged={(e) => {
+              setFirstName(e.value);
+              console.log(e.value);
+            }}
+            height={50}
+          >
+            <Validator>
+              <RequiredRule message="Name is required" />
+              <PatternRule
+                message="Do not use digits in the Name"
+                pattern={namePattern}
+              />
+              <StringLengthRule
+                message="Name must have at least 2 symbols"
+                min={2}
+              />
+            </Validator>
+          </TextBox>
+
+          <br />
+          <TextBox
+            label="Email Address"
+            labelMode="floating"
+            defaultValue={email}
+            onValueChanged={(e) => {
+              setEmail(e.value);
+              console.log(e.value);
+            }}
+            height={50}
+          >
+            <Validator>
+              <RequiredRule message="Email is required" />
+              <EmailRule message="Email is invalid" />
+            </Validator>
+          </TextBox>
+
+          <br />
+          <TextBox
+            label="Phone Number"
+            labelMode="floating"
+            maskInvalidMessage="The phone must have a correct phone format"
+            validationMessagePosition="left"
+            maskRules={phoneRules}
+            // defaultValue={phone}
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+            // onValueChanged={(e) => {
+            //   setPhone(e.value);
+            //   console.log(e.value);
+            // }}
+            height={50}
+          >
+            <Validator>
+              <PatternRule
+                message="The phone must have a correct USA phone format"
+                pattern={phonePattern}
+              />
+            </Validator>
+          </TextBox>
+        <div id="recaptcha-container"></div>
+        <Button
+        onClick={getOtp}
+        // fullWidth
+        style={{
+          backgroundColor: "#0B2F8A",
+          color: "white",
+          boxShadow: "0px 8px 24px -2px rgba(11, 47, 138, 0.6)",
+          borderRadius: "16px",
+        }}
+      >
+        Send Otp
+      </Button>
+          <br />
+          <TextBox
+            label="Company Name"
+            labelMode="floating"
+            defaultValue={companyName}
+            onValueChanged={(e) => {
+              setCompanyName(e.value);
+              console.log(e.value);
+            }}
+            height={50}
+          >
+            <Validator>
+              <RequiredRule message="Company Name is required" />
+              <StringLengthRule
+                message="Company Name must have at least 3 symbols"
+                min={3}
+              />
+            </Validator>
+          </TextBox>
+
+          <br />
+        </GroupItem>
+        <GroupItem
+          caption=""
           //style={{ padding: "20px", backgroundColor: "black" }}
+        >
+          <TextBox
+            label="Last Name"
+            labelMode="floating"
+            defaultValue={lastName}
+            onValueChanged={(e) => {
+              setLastName(e.value);
+              console.log(e.value);
+            }}
+            height={50}
           >
-            <TextBox
-              label="First Name"
-              labelMode="floating"
-              defaultValue={firstName}
-              onValueChanged={(e) => {
-                setFirstName(e.value);
-                console.log(e.value);
-              }}
-              height={50}
-            >
-              <Validator>
-                <RequiredRule message="Name is required" />
-                <PatternRule
-                  message="Do not use digits in the Name"
-                  pattern={namePattern}
-                />
-                <StringLengthRule
-                  message="Name must have at least 2 symbols"
-                  min={2}
-                />
-              </Validator>
-            </TextBox>
+            <Validator>
+              <RequiredRule message="Name is required" />
+              <PatternRule
+                message="Do not use digits in the Name"
+                pattern={namePattern}
+              />
+              <StringLengthRule
+                message="Name must have at least 2 symbols"
+                min={2}
+              />
+            </Validator>
+          </TextBox>
 
-            <br />
-            <TextBox
-              label="Email Address"
-              labelMode="floating"
-              defaultValue={email}
-              onValueChanged={(e) => {
-                setEmail(e.value);
-                console.log(e.value);
-              }}
-              height={50}
-            >
-              <Validator>
-                <RequiredRule message="Email is required" />
-                <EmailRule message="Email is invalid" />
-              </Validator>
-            </TextBox>
-
-            <br />
-            <TextBox
-              label="Phone Number"
-              labelMode="floating"
-              mask="+1 (X00) 000-0000"
-              maskInvalidMessage="The phone must have a correct phone format"
-              validationMessagePosition="left"
-              maskRules={phoneRules}
-              defaultValue={phone}
-              onValueChanged={(e) => {
-                setPhone(e.value);
-                console.log(e.value);
-              }}
-              height={50}
-            >
-              <Validator>
-                <PatternRule
-                  message="The phone must have a correct USA phone format"
-                  pattern={phonePattern}
-                />
-              </Validator>
-            </TextBox>
-            <br />
-            <TextBox
-              label="Company Name"
-              labelMode="floating"
-              defaultValue={companyName}
-              onValueChanged={(e) => {
-                setCompanyName(e.value);
-                console.log(e.value);
-              }}
-              height={50}
-            >
-              <Validator>
-                <RequiredRule message="Company Name is required" />
-                <StringLengthRule
-                  message="Company Name must have at least 3 symbols"
-                  min={3}
-                />
-              </Validator>
-            </TextBox>
-
-            <br />
-          </GroupItem>
-          <GroupItem
-            caption=""
-            //style={{ padding: "20px", backgroundColor: "black" }}
+          <br />
+          <TextBox
+            label="Add Guest Email(s)"
+            labelMode="floating"
+            defaultValue={addGuestEmail}
+            onValueChanged={(e) => {
+              setAddGuestEmail(e.value);
+              console.log(e.value);
+            }}
+            height={50}
           >
-            <TextBox
-              label="Last Name"
-              labelMode="floating"
-              defaultValue={lastName}
-              onValueChanged={(e) => {
-                setLastName(e.value);
-                console.log(e.value);
+            <Validator>
+              <RequiredRule message="Email is required" />
+              <EmailRule message="Email is invalid" />
+            </Validator>
+          </TextBox>
+          <br />
+          <TextBox
+            label="Enter OTP"
+            labelMode="floating"
+            // defaultValue={enterOtp}
+            // onValueChanged={(e) => {
+            //   setEnterOtp(e.value);
+            //   console.log(e.value);
+            // }}
+            value={otp}
+            onChange={(e) => setOtp(e.target.value)}
+            height={50}
+          >
+            <Validator>
+              <StringLengthRule message="OTP must contain 6 digits" max={6} />
+              <NumericRule message="OTP contains only Numbers" />
+            </Validator>
+          </TextBox>
+          <br />
+          <Button
+              onClick={verifyOtp}
+              fullWidth
+              style={{
+                backgroundColor: "#0B2F8A",
+                color: "white",
+                boxShadow: "0px 8px 24px -2px rgba(11, 47, 138, 0.6)",
+                borderRadius: "16px",
               }}
-              height={50}
             >
-              <Validator>
-                <RequiredRule message="Name is required" />
-                <PatternRule
-                  message="Do not use digits in the Name"
-                  pattern={namePattern}
-                />
-                <StringLengthRule
-                  message="Name must have at least 2 symbols"
-                  min={2}
-                />
-              </Validator>
-            </TextBox>
+              Verify Otp
+            </Button>
+          <TextBox
+            label="Subject for Scheduling this Interview with us"
+            labelMode="floating"
+            defaultValue={subject}
+            onValueChanged={(e) => {
+              setSubject(e.value);
+              console.log(e.value);
+            }}
+            height={50}
+          >
+            <Validator>
+              <RequiredRule message="Subject is required" />
+              <StringLengthRule
+                message="Subject must have at least 4 symbols"
+                min={4}
+              />
+            </Validator>
+          </TextBox>
 
-            <br />
-            <TextBox
-              label="Add Guest Email(s)"
-              labelMode="floating"
-              defaultValue={addGuestEmail}
-              onValueChanged={(e) => {
-                setAddGuestEmail(e.value);
-                console.log(e.value);
-              }}
-              height={50}
-            >
-              <Validator>
-                <RequiredRule message="Email is required" />
-                <EmailRule message="Email is invalid" />
-              </Validator>
-            </TextBox>
-            <br />
-            <TextBox
-              label="Enter OTP"
-              labelMode="floating"
-              defaultValue={enterOtp}
-              onValueChanged={(e) => {
-                setEnterOtp(e.value);
-                console.log(e.value);
-              }}
-              height={50}
-            >
-              <Validator>
-                <StringLengthRule message="OTP must contain 6 digits" max={6} />
-                <NumericRule message="OTP contains only Numbers" />
-              </Validator>
-            </TextBox>
-            <br />
-            <TextBox
-              label="Subject for Scheduling this Interview with us"
-              labelMode="floating"
-              defaultValue={subject}
-              onValueChanged={(e) => {
-                setSubject(e.value);
-                console.log(e.value);
-              }}
-              height={50}
-            >
-              <Validator>
-                <RequiredRule message="Subject is required" />
-                <StringLengthRule
-                  message="Subject must have at least 4 symbols"
-                  min={4}
-                />
-              </Validator>
-            </TextBox>
-
-            <br />
-          </GroupItem>
-        </Form>
-      </ValidationGroup>
+          <br />
+        </GroupItem>
+      </Form>
+    </ValidationGroup>
+      </div>
+     
       <Button
         width={160}
         text="Schedule Event"
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getAuth,} from "firebase/auth";
+// import { getFirestore, query, getDocs, collection, where, addDoc } from "firebase/firestore";
+// import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDmz4CEtBk8KKI1kBTD-yyt2NYk6N3JQUQ",
   authDomain: "calendar-app-7f344.firebaseapp.com",
@@ -18,4 +16,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const storage = getStorage(app);
+const auth = getAuth(app);
+// const db = getFirestore(app);
+export {auth}
